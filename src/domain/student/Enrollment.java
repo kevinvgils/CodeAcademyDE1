ebf1b19b8db6dfc4cb2adfc8ee0a7fb97a5d4e65
Kevin van Gils <kvnvangils@gmail.com>
2021-12-17 21:12:27 +0100

    temp

---
+++ src/domain/student/Enrollment.java
@@ -0,0 +1,5 @@
+package domain.student;
+
+public class Enrollment {
+
+}
---
+++ src/domain/student/Student.java
@@ -0,0 +1,82 @@
+package domain.student;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Student {
+    private String email;
+    private String name;
+    private Date dateOfBirth;
+    private Integer gender;
+    private String address;
+    private String city;
+    private String country;
+    private ArrayList<Enrollment> enrollments;
+
+    public Student(String email, String name, Date dateOfBirth, Integer gender, String address, String city,
+            String country) {
+        this.email = email;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.address = address;
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
